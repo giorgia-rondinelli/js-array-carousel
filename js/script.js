a1c1b29e9b832a1cbdc055bb3a1de33ac69333b0
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-26 18:36:29 +0100

    aggiunte thumbnails

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 const containerImg= document.querySelector('.container-img')
 const up= document.querySelector('.up')
 const down= document.querySelector('.down')
-
+const sideImg= document.querySelector('.side-container')
 // contatore 
 let counterImg = 0
-
+console.log(sideImg)
 
 // array 
 const images= ['img/01.webp', 'img/02.webp', 'img/03.webp','img/04.webp', 'img/05.webp']
@@ -15,16 +15,19 @@ for(i=0; i<images.length; i++){
   const img= images[i];
   containerImg.innerHTML+=`<img class= " image hide  " src="${img}"></img>`
 
+  sideImg.innerHTML+=`<img class= " side-img dark " src="${img}"></img>`
  }
 
 //  prendo elementi con classe img
 const imgCollection= document.getElementsByClassName('image')
+const sideCollection= document.getElementsByClassName('side-img')
 
 console.log(imgCollection)
 
 // rendo la prima immagine visibile
 imgCollection[counterImg].classList.remove('hide')
 
+sideCollection[counterImg].classList.remove('dark')
  console.log(up)
  
  down.classList.add('hide')
@@ -34,15 +37,19 @@ imgCollection[counterImg].classList.remove('hide')
 up.addEventListener('click', function(){
   down.classList.remove('hide')
   imgCollection[counterImg].classList.add('hide')
+  sideCollection[counterImg].classList.add('dark')
+
   counterImg++
   if(counterImg===images.length-1){
     up.classList.add('hide')
+
     
   }
   
-  imgCollection[counterImg].classList.remove('hide')
-
   
+  imgCollection[counterImg].classList.remove('hide')
+  
+  sideCollection[counterImg].classList.remove('dark')
 
 })
 
@@ -56,6 +63,7 @@ down.addEventListener('click', function(){
    
 
   imgCollection[counterImg].classList.add('hide')
+  sideCollection[counterImg].classList.add('dark')
   
   counterImg-- 
   if(counterImg===0){
@@ -64,7 +72,7 @@ down.addEventListener('click', function(){
     } 
  
   imgCollection[counterImg].classList.remove('hide')
-
+  sideCollection[counterImg].classList.remove('dark')
  
  
 })
